98212d9fd99e49e9c6cb6124c7e680b73872c5c0
Joao Sousa <j.sousa@xgeeks.io>
2020-05-26 15:36:17 +0100

    Return correct number of items

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -39,6 +39,7 @@ namespace TailSpin.SpaceGame.Web
             return Task<T>.FromResult(_items.Single(item => item.Id == id));
         }
 
+ 
         /// <summary>
         /// Retrieves items from the store that match the given query predicate.
         /// Results are given in descending order by the given ordering predicate.
